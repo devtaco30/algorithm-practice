10cccc1ade23e142e1929b2c104dd6ba8d97f6bc
devtaco30 <77915287+devtaco30@users.noreply.github.com>
2026-03-09 23:34:28 +0900

    Title: 오름 내림차순 정렬, Time: 304ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260309/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr2);
+        for (Integer i : arr2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n");
+
+        Arrays.sort(arr2, Collections.reverseOrder());
+        for (Integer i : arr2) {
+            System.out.print(i + " ");
+        }
+    }
+}
